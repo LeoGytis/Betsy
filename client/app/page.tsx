cb6f9695abb0c5e1821d9d3bc23c4137702c4e7d
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-02-10 19:37:13 +0200

    save

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import BetForm from "./components/BetForm";
 import ListView from "./components/ListView";
@@ -28,18 +29,32 @@ const HomePage: React.FC = () => {
   return (
     <>
       <NavBar userName={userName} />
-      <div className="w-full flex gap-8 mt-6">
-        {userName ? (
-          <>
-            <div className="w-1/2 flex flex-col gap-4">
-              <BetForm />
-            </div>
-            <ListView />
-          </>
-        ) : (
-          <div>Welcome to Betsy!</div>
-        )}
-      </div>
+
+      {userName ? (
+        <div className="w-full flex gap-8 mt-6">
+          <div className="w-1/2 flex flex-col gap-4">
+            <BetForm />
+          </div>
+          <ListView />
+        </div>
+      ) : (
+        <div className="realtive h-96 w-96 mx-auto flex flex-col gap-8 justify-center items-center border-2 mt-6 p-2">
+          <Image
+            src="/betsy.svg"
+            alt="betsy_image"
+            // fill
+            // objectFit="contain"
+
+            width={500}
+            height={500}
+            layout="intrinsic"
+            className="color-primary"
+          />
+          <h1 className="text-6xl font-bold text-primary whitespace-nowrap">
+            WELCOME TO BETSY!
+          </h1>
+        </div>
+      )}
     </>
   );
 };
