92b086cf57651114041f7a3b2e26573ec1e93161
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-02-10 18:21:22 +0200

    save

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      borderColor: {
+        custom: "var(--color-border)",
+      },
     },
   },
   plugins: [],
