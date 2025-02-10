10afe6dd0990f582cfd82cf9535ee1c31252237d
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-02-10 16:00:13 +0200

    change of styles

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,12 +1,32 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Metadata } from "next";
 import { Quicksand } from "next/font/google";
-import "./globals.css";
 import { BalanceProvider } from "./hooks/useBalance";
+import "./styles/globals.css";
 
 const font = Quicksand({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "Website Title",
+  description: "Website description",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/images/bet_logo.png",
+        href: "/images/bet_logo.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/images/bet_logo.png",
+        href: "/images/bet_logo.png",
+      },
+    ],
+  },
+};
 
 const queryClient = new QueryClient();
 
